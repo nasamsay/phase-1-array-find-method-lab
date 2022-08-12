5eb1f8922f5bd2034197d7f61db5d08fb2321de2
Nasam Sayour <nasamsay@gmail.com>
2022-08-12 14:18:42 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// code your solution here
+//Using find method to iterate on an array
+
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+//callback function
+const isWin = obj => { 
+        if(obj['result']=='W'){
+            return obj['year']
+        }
+    }
+   
+
+
+function superbowlWin(arr){
+    if (arr.find(isWin) == undefined){
+        return undefined
+    }
+    else{
+        return arr.find(isWin)['year']
+    }
+    
+}
+
+console.log(superbowlWin(record))
+console.log(isWin(record[1]))
+
+const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" }]
+console.log(superbowlWin(sadReality))
